7004f83b26138fa47ff5b36c7b554f22d4e48e5b
usnijee <bluesea9519@gmail.com>
2024-06-21 19:51:55 +0900

    CREATE: Flask backend server project skeleton, directory structure

---
+++ FlaskBE/app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from .extensions import db
+from .config import Config
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # Initialize extensions
+    db.init_app(app)
+
+    # Register blueprints
+
+    return app
