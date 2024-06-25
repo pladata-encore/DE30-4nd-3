733dbb6c0372625181a2a81c381a74c2d6e409e6
kunghyun6357 <kung6357@gmail.com>
2024-06-25 10:08:34 +0900

    Modified : Add Comment to function of each py file

--- FlaskBE/modules/PUBGAnalyzer.py
+++ FlaskBE/modules/PUBGAnalyzer.py
@@ -9,6 +9,7 @@ class PubgAnalyzer:
         self.victim_times = self.get_victim_times()
         self.coordinate_list = []
     
+    # 중복제거를 위해 이름을 set()형식으로 담고 list로 반환
     def get_all_player_names(self):
         player_names = set()
         for entry in self.data:
@@ -19,6 +20,8 @@ class PubgAnalyzer:
                 pass
         return list(player_names)
     
+    # 엔트리는 data의 각 행
+    # 페이즈가 바뀌는 시점을 리스트에 저장하는데, 페이즈의 값이 정수인 것만(그 시점이 자기장이 생성되는 시점이므로) 반환하는 코드
     def get_phase_changes(self):
         phase_changes = []
         for entry in self.data:
@@ -29,6 +32,7 @@ class PubgAnalyzer:
                 pass
         return phase_changes
     
+    # 생존자 판별을 위해 킬로그를 분석하고 victim에서 해당 이름과 죽은 시간을 추출
     def get_victim_times(self):
         victim_times = []
         for entry in self.data:
@@ -39,6 +43,7 @@ class PubgAnalyzer:
                 pass
         return victim_times
     
+    # 시간 형식이 전부 스트링이므로 datetime 형식으로 전환후에 해당 시간 범주 내의 생존자들의 좌표만 리스트로 반환
     def calculate_geometric_center(self):
         for phase, phase_time_str in self.phase_changes:
             phase_time = datetime.strptime(phase_time_str, '%Y-%m-%dT%H:%M:%S.%fZ')
@@ -48,6 +53,7 @@ class PubgAnalyzer:
             # print(f"{phase}페이즈 기하학적 중심 {(x, y)}")
         return self.coordinate_list
     
+    # 생존자 추출을 위해 페이즈별 시간대마다 인원 리스트에서 희생자들 이름 제거 
     def filter_alive_players(self, phase_time):
         alive_players = self.initial_player_names[:]
         victim_datetime_list = [
@@ -59,6 +65,7 @@ class PubgAnalyzer:
                 alive_players.remove(name)
         return alive_players
     
+    # 페이즈별 센트로이드 계산
     def calculate_center_for_phase(self, alive_players, phase_time):
         x_sum, y_sum = 0, 0
         count = 0
@@ -79,6 +86,7 @@ class PubgAnalyzer:
             x, y = 0, 0
         return x, y
     
+    # 원하는 시간대의 범주를 구하기 위한 코드
     def find_closest_log_time(self, player, phase_time):
         log_times = [
             entry['_D']
@@ -95,6 +103,7 @@ class PubgAnalyzer:
                 closest_time = ts
         return closest_time
     
+    # 실제 자기장의 좌표와 크기를 구하는 함수
     def get_gas_positions(self):
         gas_list = []
         for entry in self.data:
@@ -108,6 +117,7 @@ class PubgAnalyzer:
                 pass
         return gas_list
     
+    # 자기장의 x,y 좌표를 구하는 함수
     def print_gas_positions(self):
         gas_list = self.get_gas_positions()
         gas_coords_list = []
--- FlaskBE/modules/PostJsonToAPI.py
+++ FlaskBE/modules/PostJsonToAPI.py
@@ -1,5 +1,6 @@
 import requests
 
+# 분석모듈로 분석된 데이터를 DB에 API로 전송하여 적재하는 모듈
 class PostJsonToApi:
     def __init__(self, url, endPoint, phaseNum, matchId, user_x, user_y, real_x, real_y):
         self.url = url
@@ -14,13 +15,14 @@ class PostJsonToApi:
             "table": f"phase{self.phaseNum}",
             "data": {
                 "match_id": str(self.matchId),
-                "user_geometry_center_x": float(self.user_x),
+                "user_geometry_center_x": float(self.user_x),   
                 "user_geometry_center_y": float(self.user_y),
                 "white_zone_center_x": float(self.real_x),
                 "white_zone_center_y": float(self.real_y)
             }
         }
     
+    # 디버깅용 프린트, POST 요청으로 데이터를 전송
     def postData(self):
         headers = {'Content-Type': 'application/json'}
         url = self.url + self.endPoint
@@ -28,7 +30,7 @@ class PostJsonToApi:
         print(f"매치 아이디 : {self.matchId}")
         print(f"페이즈 남바 : {self.phaseNum}")
         print(f"바디 : {self.body}")
-        if response.status_code == 204:
+        if response.status_code == 200:
             print(f"Response content: {response.content}")
         elif response.status_code == 404:
             print(f"Response content: {response.content}")
