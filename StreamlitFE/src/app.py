500f82f451dfb45e62f9e216969c8af07e301e3b
kunghyun6357 <kung6357@gmail.com>
2024-06-27 20:34:26 +0900

    MODIFIED : New Modules added, update app.py

--- StreamlitFE/src/app.py
+++ StreamlitFE/src/app.py
@@ -1,11 +1,16 @@
 import streamlit as st
 from main import main
 from report import report
+from pubgstats import PUBGStatsApp
 
 st.sidebar.title("Menu")
-page = st.sidebar.selectbox("Pages move on", ["Main", "Analysis Results"])
+page = st.sidebar.selectbox("Pages move on", ["Main", "Analysis Results","Search Match History"])
 
 if page == "Main":
     main()
 elif page == "Analysis Results":
     report()
+elif page == "Search Match History":
+    if __name__ == "__main__":
+        app = PUBGStatsApp()
+        app.run()
